daa95876e018870c66fdb89f69b24c7de5f11aee
Dmitry Berezhnoy <d1m-berezhnoy@yandex.ru>
2022-10-24 21:32:30 +0400

    loginSlice unit tests

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+
+import { ILoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+  test('set username', () => {
+    const state: DeepPartial<ILoginSchema> = { username: '' };
+    expect(loginReducer(state as ILoginSchema, loginActions.setUsername('username test')))
+      .toEqual({ username: 'username test' });
+  });
+
+  test('set password', () => {
+    const state: DeepPartial<ILoginSchema> = { password: '' };
+    expect(loginReducer(state as ILoginSchema, loginActions.setPassword('password test')))
+      .toEqual({ password: 'password test' });
+  });
+});
