85b5a6ede673b4870f3c8aae0f429265f162254a
Dmitry Berezhnoy <d1m-berezhnoy@yandex.ru>
2023-06-26 23:13:14 +0400

    fix storybook

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,9 +1,10 @@
 import { addDecorator } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
-import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
-import { SuspenseDecorator } from 'shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import ThemeDecorator from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Theme } from '@/app/providers/ThemeProvider';
+import { RouterDecorator } from '@/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '@/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { SuspenseDecorator } from '@/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import ThemeDecorator from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -16,6 +16,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
   };
   config.resolve!.modules!.push(paths.src);
   config.resolve!.extensions!.push('.ts', '.tsx');
+  config.resolve!.alias = {
+    ...config.resolve!.alias,
+    '@': paths.src,
+  };
 
   // Для замены стандартного svg лоадера storybook
   // eslint-disable-next-line no-param-reassign
